96d9c62b0e52b8ab4f7845c541d6da6440bf43bc
Pirumyan <138876991+Pirumyan@users.noreply.github.com>
2023-10-04 00:40:06 +0400

    Add files via upload

---
+++ nor_pordz.py
@@ -0,0 +1,41 @@
+def openchopen(namefile, mode='r'):
+    if mode == 'r':
+        file = open(f'{namefile}')
+        print(file.read())
+        file.close()
+
+    elif mode == 'w' or mode == 'a':
+        file = open(f'{namefile}', f'{mode}')
+        file.write(input("ste gri qo texty \n"))
+        file.close()
+
+    elif mode == 'w+':
+        file = open(f'{namefile}', f'{mode}')
+        file.write(input("ste gri qo texty \n"))
+        file.seek(0)
+        print(file.read())
+        file.close()
+
+    elif mode == 'r+':
+        file = open(f'{namefile}', f'{mode}')
+        print(file.read())
+        file.write(input("Gri qo nor texty fili mej\n"))
+        file.close()
+    elif mode == "a+":
+        file = open(f'{namefile}', f'{mode}')
+        file.write(input("Gri en texty vor avenalu a \n"))
+        file.seek(0)
+        print(file.read())
+        file.close()
+
+    else:
+        print('menq chuneq tenc mode hajoxutyun\n')
+
+file_name, rejim = map(str, input().split())
+if rejim == 'r' or rejim == 'r+':
+    try:
+        openchopen(file_name)
+    except FileNotFoundError:
+        print(' unas chka tenc file')
+else:
+    openchopen(file_name, mode=rejim)
